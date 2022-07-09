a97c35cf18e638af433a087fb6246c64ee3730da
YEAHx4 <developerstarlight@gmail.com>
2022-07-09 09:36:21 +0900

    draw box

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
-const start = () => {
-  window.canvas = document.getElementById("canvas");
+class Box {
+  x = 10;
+  y = 10;
+  width = 50;
+  height = 50;
+  color = "black";
+
+  draw() {
+    console.log("draw!");
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
+
+const simulate = () => {
+  console.log("simulating");
+  const box = new Box();
+  box.draw();
 };
 
-window.onload(start);
+const start = () => {
+  console.log("start");
+  window.canvas = document.getElementById("canvas");
+  window.ctx = canvas.getContext("2d");
+  window.reset = document.getElementById("reset-btn");
+
+  ctx.canvas.width = 1024;
+  ctx.canvas.height = 1024;
+
+  reset.onclick = simulate;
+};
+
+window.onload = start;
